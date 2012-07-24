0d2e61ad57d798c1c6d81f401e87bfde552121e3
Chris Pettitt <chris@samsarin.com>
2012-07-23 20:25:17 -0700

    Expose attachId on dig.util.

--- test/util-test.js
+++ test/util-test.js
@@ -2,6 +2,38 @@ var assert = require('assert'),
     util = require('../index').util;
 
 describe('dig.util', function() {
+  describe('attachId', function() {
+    it('should attach an id to an object', function() {
+      var n = {};
+      util.attachId(n);
+      assert.notEqual(undefined, n._digId);
+    });
+
+    it('should attach an id to an array', function() {
+      var n = [];
+      util.attachId(n);
+      assert.notEqual(undefined, n._digId);
+    });
+
+    it('should not attach an id to a number', function() {
+      assert.throws(function() { util.attachId(1); });
+    });
+
+    it('should not attach an id to a string', function() {
+      assert.throws(function() { util.attachId("abc"); });
+    });
+
+    it('should generate different id on successive iterations', function() {
+      var seen = {};
+      for (var i = 0; i < 50; ++i) {
+        var n = {};
+        util.attachId(n);
+        assert.equal(undefined, seen[n._digId]);
+        seen[n._digId] = true; 
+      }
+    });
+  });
+
   describe('isArray', function() {
     it('should return `true` for an array', function() {
       assert.ok(util.isArray([]));
