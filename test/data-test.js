46ad53a90f60cde0304a7140f3bd6b6bc4292939
Chris Pettitt <chris@samsarin.com>
2012-07-24 21:27:30 -0700

    Add a binary heap implementation

--- test/data-test.js
+++ test/data-test.js
@@ -35,4 +35,65 @@ describe('dig.data', function() {
       assert.equal(0, q.size());
     });
   });
+
+  describe('binaryHeap', function() {
+    it('should allow no-arg construction', function() {
+      assert.equal(0, data.binaryHeap().size());
+    });
+
+    it('should allow nodes to be added', function() {
+      var h = data.binaryHeap();
+      h.add(5);
+      assert.equal(1, h.size());
+      h.add(3);
+      assert.equal(2, h.size()); 
+    });
+
+    it('should return the smallest value with `min`', function() {
+      var h = data.binaryHeap();
+      h.add(5);
+      h.add(3);
+      assert.equal(3, h.min());
+    });
+
+    it('should remove the smallest value with `removeMin`', function() {
+      var h = data.binaryHeap();
+      h.add(5);
+      h.add(3);
+      assert.equal(3, h.removeMin());
+      assert.equal(5, h.removeMin());
+      assert.equal(0, h.size());      
+    });
+
+    it('should throw an error with an empty heap and `removeMin`', function() {
+      assert.throws(function() {
+        data.binaryHeap().removeMin();
+      });
+    });
+
+    it('should properly order a sequence of random numbers', function() {
+      var nums = [];
+      var h = data.binaryHeap();
+      var num;
+      for (var i = 0; i < 500; ++i) {
+        num = Math.floor(Math.random() * 1000);
+        nums.push(num);
+        h.add(num);
+      }
+
+      var actual = [];
+      while (h.size() > 0) {
+        actual.push(h.removeMin());
+      }
+
+      assert.deepEqual(nums.sort(function(l, r) { return l - r; }), actual);
+    });
+  });
+
+  it('should allow construction with an initial array', function() {
+    var h = data.binaryHeap([5, 3, 7]);
+    assert.equal(3, h.removeMin());
+    assert.equal(5, h.removeMin());
+    assert.equal(7, h.removeMin());
+  });
 });
