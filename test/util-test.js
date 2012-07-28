e565ab8e0d4393ba8d0fe544f00f22832863d459
Chris Pettitt <chris@samsarin.com>
2012-07-28 03:05:40 -0700

    Redefine map + some dig.util cleanup

--- test/util-test.js
+++ test/util-test.js
@@ -49,20 +49,32 @@ describe('dig.util', function() {
   });
 
   describe('map', function() {
+    function id(x) { return x; }
+
     it('should return an empty array when given an empty array', function() {
-      assert.deepEqual([], dig.util.map(function(x) { return x; }, []));
+      assert.deepEqual([], dig.util.map(id, []));
     });
 
     it('should map each element of the given array', function() {
       assert.deepEqual([1,2,3], dig.util.map(function(x) { return x + 1; }, [0, 1, 2]));
     });
 
-    it('should throw when given a non-function', function() {
-      assert.throws(function() { dig.util.map("not-a-function", [0, 1, 2]); });
+    it('should handle objects with enumerable fields', function() {
+      assert.deepEqual(['t', 'e', 's', 't'], dig.util.map(id, "test"));
+      assert.deepEqual([1,2,3,4], dig.util.map(id, {a: 1, b: 2, d: 3, c: 4}).sort());
     });
 
-    it('should throw when given a non-array', function() {
-      assert.throws(function() { dig.util.map(function(x) { return x; }, {a: 123}); });
+    it('should handle objects with prototypes', function() {
+      function obj() {
+        this.a = 1;
+        this.b = 2;
+      }
+      obj.prototype.c = 3;
+      assert.deepEqual([1, 2, 3], dig.util.map(id, new obj()).sort());
+    });
+
+    it('should throw when given a non-function', function() {
+      assert.throws(function() { dig.util.map("not-a-function", [0, 1, 2]); });
     });
   });
 });
