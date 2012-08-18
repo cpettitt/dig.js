aed0a36d0627a1c8330b7c0dcf2376cacb8cd59e
Chris Pettitt <chris@samsarin.com>
2012-08-18 03:32:28 -0700

    Use dot graphs for prim's tests

--- test/dig/alg/prim-test.js
+++ test/dig/alg/prim-test.js
@@ -1,5 +1,7 @@
 require("../../test-env");
 
+graphs = require("../../test-graphs");
+
 function edgeLabel(g) {
   return function(u, v) {
     return g.hasEdge(u, v) ? g.edgeLabel(u, v) : Number.POSITIVE_INFINITY;
@@ -8,50 +10,22 @@ function edgeLabel(g) {
 
 describe("dig.alg.prim", function() {
   it("returns an empty graph given an empty graph", function() {
-    var g = new dig.UGraph();
+    var g = graphs.undirected.empty;
     assert.graphEqual(g, dig.alg.prim(g));
   });
 
   it("returns a single node for a singleton graph", function() {
-    var g = new dig.UGraph();
-    g.addNode(1);
+    var g = graphs.undirected.node1;
     assert.graphEqual(g, dig.alg.prim(g));
   });
 
-  it("returns a unique result for diamond graph with different path weights", function() {
-    var g = new dig.UGraph();
-    g.addNodes(1, 2, 3, 4);
-    g.addEdge(1, 2, 1);
-    g.addEdge(1, 3, 2);
-    g.addEdge(2, 4, 3);
-    g.addEdge(3, 4, 4);
-
-    var expected = new dig.UGraph();
-    expected.addNodes(1, 2, 3, 4);
-    expected.addPath(1, 2, 4);
-    expected.addEdge(1, 3);
-
-    assert.graphEqual(expected, dig.alg.prim(g, edgeLabel(g)));
-  });
-
   it("returns a unique result for a small network with different path weights", function() {
-    var g = new dig.UGraph();
-    g.addNodes(1, 2, 3, 4, 5, 6);
-    g.addEdge(1, 2, 1);
-    g.addEdge(1, 3, 3);
-    g.addEdge(1, 6, 5);
-    g.addEdge(2, 3, 9);
-    g.addEdge(2, 4, 2);
-    g.addEdge(3, 4, 8);
-    g.addEdge(3, 5, 4);
-    g.addEdge(3, 6, 6);
-    g.addEdge(4, 5, 7);
+    var g = graphs.undirected.prim6;
 
     var expected = new dig.UGraph();
-    expected.addNodes(1, 2, 3, 4, 5, 6);
-    expected.addPath(1, 2, 4);
-    expected.addPath(1, 3, 5);
-    expected.addEdge(1, 6);
+    expected.addNodes("A", "B", "C", "D", "E", "F");
+    expected.addPath("A", "B", "C");
+    expected.addPath("B", "D", "E", "F");
 
     assert.graphEqual(expected, dig.alg.prim(g, edgeLabel(g)));
   });
