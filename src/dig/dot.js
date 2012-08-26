c67badb1de18525b1c4f4737e927d2aa93554477
Chris Pettitt <chris@samsarin.com>
2012-08-25 20:15:07 -0700

    Some rework to dot layout pipeline

--- src/dig/dot.js
+++ src/dig/dot.js
@@ -78,6 +78,16 @@ var dig_dot_read = dig.dot.read = function(dot) {
 }
 
 /*
+ * Lays out the given graph.
+ *
+ * NOTE: this is a work in progress.
+ */
+var dig_dot_layout = dig.dot.layout = function(g) {
+  var ranked = dig_dot_alg_rank(g);
+  return dig_dot_alg_order(ranked);
+}
+
+/*
  * Returns an array of ranks, with each rank contains an array of nodes in the
  * rank. Nodes in each rank do not have a particular order.
  *
@@ -86,7 +96,7 @@ var dig_dot_read = dig.dot.read = function(dot) {
  *  1. Input graph is connected
  */
 var dig_dot_alg_rank = dig.dot.alg.rank = function(g) {
-  var acyclic = dig_dot_alg_makeAcyclic(g);
+  var acyclic = dig_dot_alg_acyclic(g);
   return dig_dot_alg_initRank(acyclic);
 }
 
@@ -135,7 +145,7 @@ var dig_dot_alg_acyclic = dig.dot.alg.acyclic = function(g) {
 
 /*
  * Finds a feasible ranking (description below) for the given graph and
- * returns the ranking via an object that maps node keys to ranks.
+ * returns a copy of the graph with the nodes labelled with their rank.
  *
  * A feasible ranking is one such that for all edges e, length(e) >=
  * min_length(e). For our purposes min_length(e) is always 1 and length(e)
@@ -147,18 +157,18 @@ var dig_dot_alg_acyclic = dig.dot.alg.acyclic = function(g) {
  *  2. Input graph is acyclic
  */
 var dig_dot_alg_initRank = dig.dot.alg.initRank = function(g) {
+  g = g.copy();
   var pq = new dig_data_PriorityQueue();
   dig_util_forEach(g.nodes(), function(u) {
     pq.add(u, g.indegree(u));
   });
 
-  var ranks = {};
   var current = [];
   var rankNum = 0;
   while (pq.size() > 0) {
     for (var min = pq.min(); pq.priority(min) === 0; min = pq.min()) {
       pq.removeMin();
-      ranks[min] = rankNum;
+      g.nodeLabel(min, rankNum);
       current.push(min);
     }
 
@@ -176,18 +186,51 @@ var dig_dot_alg_initRank = dig.dot.alg.initRank = function(g) {
     rankNum++;
   }
 
-  return ranks;
+  return g;
 }
 
 /*
- * Given a graph and a map of nodes to ranks, this function returns an array
+ * Given a graph with nodes labelled by their rank, this function returns an array
  * of ranks where each rank has nodes ordered to minimize edge crossings.
  *
  * NOTE: this is a work in progress
  */
-var dig_dot_alg_order = dig.dot.alg.order = function(g, ranks) {
-  return dig_dot_alg_initOrder(g, ranks);
+var dig_dot_alg_order = dig.dot.alg.order = function(g) {
+  var g2 = dig_dot_alg_addDummyNodes(g);
+  var ranks = dig_dot_alg_initOrder(g2);
+  return {ranks: ranks, graph: g2};
 }
+
+/*
+ * Given a graph with nodes labelled by their rank, this function returns a new
+ * graph where each edge is of unit length. For example, if rank(u) = 2 and
+ * rank(v) = 4 and there is an edge (u, v), this function will replace the
+ * edge (u, v) with two edges (u, w), (w, v) so that no edge has a length
+ * greater than 1. In this example, rank(w) = 1.
+ */
+var dig_dot_alg_addDummyNodes = dig.dot.alg.addDummyNodes = function(g) {
+  g = g.copy();
+  var dummyCount = 0;
+  dig_util_forEach(g.edges(), function(e) {
+    var origU = e.from,
+        u = e.from,
+        v = e.to,
+        rankU = g.nodeLabel(u) + 1,
+        rankV = g.nodeLabel(v);
+    g.removeEdge(u, v);
+    while (rankU < rankV) {
+      var w = "_dummy-" + origU + "-" + v + "-" + dummyCount++;
+      g.addNode(w);
+      g.nodeLabel(w, rankU);
+      g.addEdge(u, w);
+      u = w;
+      rankU++;
+    }
+    g.addEdge(u, v);
+  });
+  return g;
+}
+
 
 /*
  * Returns an array of ranks where each rank has a list of nodes in the given
@@ -195,10 +238,10 @@ var dig_dot_alg_order = dig.dot.alg.order = function(g, ranks) {
  * which to generate an ordering with minimal edge crossings, but almost
  * certainly some iteration will reduce edge crossing.
  */
-var dig_dot_alg_initOrder = dig.dot.alg.initOrder = function(g, ranks) {
+var dig_dot_alg_initOrder = dig.dot.alg.initOrder = function(g) {
   // We currently use DFS as described in the graphviz paper.
 
-  var rankArr = [];
+  var ranks = [];
   var visited = {};
 
   function dfs(u) {
@@ -207,8 +250,8 @@ var dig_dot_alg_initOrder = dig.dot.alg.initOrder = function(g, ranks) {
     }
     visited[u] = true;
 
-    var rankNum = ranks[u];
-    var rank = (rankArr[rankNum] = rankArr[rankNum] || []);
+    var rankNum = g.nodeLabel(u);
+    var rank = (ranks[rankNum] = ranks[rankNum] || []);
     rank.push(u);
 
     dig_util_forEach(g.successors(u), function(v) {
@@ -217,10 +260,10 @@ var dig_dot_alg_initOrder = dig.dot.alg.initOrder = function(g, ranks) {
   }
 
   dig_util_forEach(g.nodes(), function(u) {
-    if (ranks[u] === 0) {
+    if (g.nodeLabel(u) === 0) {
       dfs(u);
     }
   });
 
-  return rankArr;
+  return ranks;
 }
--- test/dig/dot-test.js
+++ test/dig/dot-test.js
@@ -77,20 +77,34 @@ describe("dig.dot.alg.acyclic(graph)", function() {
 describe("dig.dot.alg.initRank(graph)", function() {
   it("can rank a singleton graph", function() {
     var g = graphs.directed.node1;
-    var ranks = dig.dot.alg.initRank(g);
-    assert.deepEqual({1: 0}, ranks);
+    var expected = g.copy();
+    expected.nodeLabel(1, 0);
+
+    var actual = dig.dot.alg.initRank(g);
+    assert.graphEqual(expected, actual);
   });
 
   it("can rank the diamond graph", function() {
     var g = graphs.directed.diamond;
-    var ranks = dig.dot.alg.initRank(g);
-    assert.deepEqual({1: 0, 2: 1, 3: 1, 4: 2}, ranks);
+    var expected = g.copy();
+    expected.nodeLabel(1, 0);
+    expected.nodeLabel(2, 1);
+    expected.nodeLabel(3, 1);
+    expected.nodeLabel(4, 2);
+
+    var actual = dig.dot.alg.initRank(g);
+    assert.graphEqual(expected, actual);
   });
 
   it("can rank a graph with multiple inedge constraints", function() {
     var g = dig.dot.read("digraph { 1 -> 2 -> 3; 1 -> 3 }");
-    var ranks = dig.dot.alg.initRank(g);
-    assert.deepEqual({1: 0, 2: 1, 3: 2}, ranks);
+    var expected = g.copy();
+    expected.nodeLabel(1, 0);
+    expected.nodeLabel(2, 1);
+    expected.nodeLabel(3, 2);
+
+    var actual = dig.dot.alg.initRank(g);
+    assert.graphEqual(expected, actual);
   });
 
   it("throws an error for undirected graphs", function() {
@@ -104,23 +118,64 @@ describe("dig.dot.alg.initRank(graph)", function() {
   });
 });
 
-describe("dig.dot.alg.initOrder(graph, ranks)", function() {
+describe("dig.dot.alg.addDummyNodes(graph)", function() {
+  it("does not change a graph with unit length edges", function() {
+    var g = graphs.directed.edge1;
+    g.nodeLabel(1, 0);
+    g.nodeLabel(2, 1);
+    var g2 = dig.dot.alg.addDummyNodes(g);
+    assert.graphEqual(g, g2);
+  });
+
+  it("inserts nodes between incident nodes separated by more than one rank", function() {
+    var g = graphs.directed.edge1;
+    g.nodeLabel(1, 0);
+    g.nodeLabel(2, 2);
+
+    var g2 = dig.dot.alg.addDummyNodes(g);
+    assert.isFalse(g2.hasEdge(1, 2));
+    var successors = g2.successors(1);
+    assert.equal(1, successors.length);
+    var successor = successors[0];
+    assert.isTrue(g2.hasEdge(successor, 2));
+  });
+
+  it("assigns the correct rank when inserting nodes", function() {
+    var g = graphs.directed.edge1;
+    g.nodeLabel(1, 0);
+    g.nodeLabel(2, 2);
+
+    var g2 = dig.dot.alg.addDummyNodes(g);
+    assert.equal(1, g2.nodeLabel(g2.successors(1)[0]));
+  });
+});
+
+describe("dig.dot.alg.initOrder(graph)", function() {
   it("returns an array ordering for graphs", function() {
     var g = graphs.directed.diamond;
-    var ranks = {1: 0, 2: 1, 3: 1, 4: 2};
-    var rankArr = dig.dot.alg.initOrder(g, ranks);
-    assert.deepEqual([1], rankArr[0]);
-    assert.deepEqual([2, 3], rankArr[1].sort());
-    assert.deepEqual([4], rankArr[2]);
-    assert.equal(3, rankArr.length);
+    g.nodeLabel(1, 0);
+    g.nodeLabel(2, 1);
+    g.nodeLabel(3, 1);
+    g.nodeLabel(4, 2);
+
+    var ranks = dig.dot.alg.initOrder(g);
+    assert.deepEqual([1], ranks[0]);
+    assert.deepEqual([2, 3], ranks[1].sort());
+    assert.deepEqual([4], ranks[2]);
+    assert.equal(3, ranks.length);
   });
 
   it("works for graphs with multiple min-rank nodes", function() {
     var g = dig.dot.read("digraph { 1 -> 3; 1 -> 4; 2 -> 5 }");
-    var ranks = {1: 0, 2: 0, 3: 1, 4: 1, 5: 1};
-    var rankArr = dig.dot.alg.initOrder(g, ranks);
-    assert.deepEqual([1, 2], rankArr[0].sort());
-    assert.deepEqual([3, 4, 5], rankArr[1].sort());
-    assert.equal(2, rankArr.length);
+    g.nodeLabel(1, 0);
+    g.nodeLabel(2, 0);
+    g.nodeLabel(3, 1);
+    g.nodeLabel(4, 1);
+    g.nodeLabel(5, 1);
+
+    var ranks = dig.dot.alg.initOrder(g, ranks);
+    assert.deepEqual([1, 2], ranks[0].sort());
+    assert.deepEqual([3, 4, 5], ranks[1].sort());
+    assert.equal(2, ranks.length);
   });
 });
